68993f7ea7cd2021dc403348fc4ec5f0f718b828
BearingMe <gomes.bruno201@gmail.com>
2025-08-16 11:19:32 -0300

    feat: add mcp.json configuration and refactor main.py for lyrics processing

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from mcp.server.fastmcp import FastMCP
 from lib import read_as_str
-
+import re
 
 mcp = FastMCP("Lyrics")
 
 
-@mcp.resource("lyric://{name}")
+@mcp.resource("lyrics://{name}")
 def retrieve_lyrics(name: str):
     relative_path = f"./src/assets/lyrics/{name}.txt"
     lyric = read_as_str(relative_path)
@@ -16,6 +16,32 @@ def retrieve_lyrics(name: str):
     return lyric.strip()
 
 
+@mcp.resource("resource://my-resource")
+def get_data() -> str:
+    return "Hello, world!"
+
+
 @mcp.tool()
-def split_lyrics(lyrics: str):
+def split_words(lyrics: str):
+    """
+    Cleans and processes a string of song lyrics by removing certain punctuation,
+    redundant whitespace, and content within brackets. The text is then normalized
+    (stripped and lowercased), and a list of unique characters is returned.
+
+    Args:
+        lyrics (str): The input song lyrics as a string.
+
+    Returns:
+        List[str]: A list of unique characters from the cleaned and lowercased lyrics.
+    """
+    lyrics = re.sub(r"[\(\[\{\<].*?[\)\]\}\>]", "", lyrics)
+    lyrics = re.sub(r"[.!?]+(?=\s|$)", "", lyrics)
+    lyrics = re.sub(r"\n", " ", lyrics)
+    lyrics = re.sub(r"\s{2,}", " ", lyrics)
+    lyrics = lyrics.strip().lower()
+
     return list(set(lyrics.split()))
+
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
